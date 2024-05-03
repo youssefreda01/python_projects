79046fc72b660b384ec3d3ddadcd990ed76eabf0
zeus_code <152637026+youssefreda01@users.noreply.github.com>
2024-05-03 18:21:58 +0300

    Update f19to20.py

--- f19to20.py
+++ f19to20.py
@@ -1,9 +1,9 @@
 #------------------------------
 # this code made by USF_reda
-# thank you eng.momen
 #------------------------------
 
 # task (1)
+print("task (1)\n")
 
 print(type(100))   
 print(type(-100))  
@@ -17,6 +17,7 @@ print(type(-.99))
 
 
 # task (2)
+print("task (2)\n")
 
 num = 1+2j
 
@@ -25,12 +26,14 @@ print("real part is : {}".format(num.real))  # 1.0
 
 
 # task (3)
+print("task (3)\n")
 
 num =10
 print("{:f}".format(num)) # 10.00000
 
 
 # task (4)
+print("task (4)\n")
 
 num = 159.650 
 
@@ -39,6 +42,7 @@ print(type(159)) # <class 'int'>
 
 
 # task (5)
+print("task (5)\n")
 
 print(100 - 115) # -15
 print(50 * 30) # 1500
